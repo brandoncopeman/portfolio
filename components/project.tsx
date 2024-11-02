97fcdff9fae1922761511c972bb5a6711ec164eb
Brandon Copeman <brancopeman@gmail.com>
2024-11-02 18:34:07 +0800

    fixed theme toggle to start as light mode

--- components/project.tsx
+++ components/project.tsx
@@ -24,7 +24,7 @@ export default function Project({ title, description, tags, imageUrl } :
     >
         <section 
     className=' bg-grey-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] h-[20rem]  hover:bg-gray-200 transition group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white'>
-       <div className='pt-4 pb-4 px-4 sm:pl-10 sm:pr-2 sm:pt-10 max-w-[50%]   flex flex-col h-full group-even:ml-[50%] '>
+       <div className='pt-4 pb-4 px-40 sm:pl-10 sm:pr-2 sm:pt-10 max-w-[50%]   flex flex-col h-full group-even:ml-[50%] '>
         <h3 className='text-2xl font-semibold'>
             {title}
         </h3>
