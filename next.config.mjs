694e9093d276dd4ecf4ac8c16f4c611ce9f10cc5
Brandon Copeman <brancopeman@gmail.com>
2024-11-04 02:42:00 +0800

    fixed nextjsyml

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 experimental: {    
-    serverActions: true}
+    serverActions: true},
+    reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix:  '/your-repository-name/'  ,
+  basePath:  '/your-repository-name'  ,
+  output: 'export'
 };
+
 
 
 export default nextConfig;
